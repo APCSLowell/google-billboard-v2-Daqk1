2bff9665aa0779129306058f47e76485f9210128
Justin Wu <111600974+Daqk1@users.noreply.github.com>
2024-01-11 12:47:55 -0800

    Update GoogleBillboard.java

--- main/java/GoogleBillboard.java
+++ main/java/GoogleBillboard.java
@@ -1,4 +1,3 @@
-
 public class Main {
     public final static String e = "2.7182818284590452353602874713526624977572470936999595749669676277240766303535475945713821785251664274274663919320030599218174135966290435";  
     public static void main(String[] args) {
@@ -13,5 +12,11 @@ public class Main {
 
     public static boolean isPrime(double dNum)  
     {   
-      
+      for(double i = 2; i <= Math.sqrt(dNum); i++){
+        if(dNum%i == 0){
+          return false;
+        }
+        }
+        return true;  
+}
 }
