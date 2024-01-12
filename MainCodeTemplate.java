ffb66702e0a2569d96fde2379a861b8c75bd629e
Justin Wu <111600974+Daqk1@users.noreply.github.com>
2024-01-12 10:51:03 -0800

    Create MainCodeTemplate.java

---
+++ MainCodeTemplate.java
@@ -0,0 +1,16 @@
+public class GoogleBillboard {
+    public final static String e = "2.7182818284590452353602874713526624977572470936999595749669676277240766303535475945713821785251664274274663919320030599218174135966290435";
+
+    public static void main(String[] args) {
+        for(int i = 2; i < e.length()-10; i++){
+        double test = Double.parseDouble(e.substring(i,i+10));
+        if(isPrime(test) == true){
+          System.out.println(test);
+          break;
+        }
+      }
+    }
+
+    public static boolean isPrime(double dNum) {
+       // Placeholder for dynamic code
+}
