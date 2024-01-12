0fc634200e8f80c67841213626813156c1a01c3f
Justin Wu <111600974+Daqk1@users.noreply.github.com>
2024-01-11 16:17:43 -0800

    Update GoogleBillboard.java

--- main/java/GoogleBillboard.java
+++ main/java/GoogleBillboard.java
@@ -1,5 +1,3 @@
-
-public class Main {
     public final static String e = "2.7182818284590452353602874713526624977572470936999595749669676277240766303535475945713821785251664274274663919320030599218174135966290435";  
     public static void main(String[] args) {
       for(int i = 2; i < e.length()-10; i++){
@@ -20,4 +18,4 @@ public class Main {
         }
         return true;  
 }
-}
+
